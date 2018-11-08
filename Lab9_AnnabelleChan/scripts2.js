e130e0a4b8c018becd68af3c4af60c59859f95e0
okokannabelle <43479832+okokannabelle@users.noreply.github.com>
2018-11-07 22:55:03 -0500

    Add files via upload

---
+++ Lab9_AnnabelleChan/scripts2.js
@@ -0,0 +1,14 @@
+//Sets the initial colours of the three elements on page load
+	document.getElementById("blue").style.color="blue";
+	document.getElementById("green").style.color="green";
+	document.getElementById("red").style.color="red";
+
+
+//A function to change colours of elements when called
+function mix(){
+
+	document.getElementById("blue").style.color="#00BB00";
+	document.getElementById("green").style.color="#BB9320";
+	document.getElementById("red").style.color="#AB00FF";
+
+}
